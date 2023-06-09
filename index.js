430870e20211df40416cd4425968455192f977c1
Lewis <lewis.kagia@student.moringaschool.com>
2023-06-09 13:11:43 +0300

    object assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution in this file!
+// Initialize the employee object
+const employee = {
+    name: "Sam",
+    streetAddress: "11 Broadway"
+  };
+  
+  // Function to update the employee object with a new key-value pair (non-destructive)
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+      ...employee,
+      [key]: value
+    };
+  }
+  
+  // Function to update the employee object with a new key-value pair (destructive)
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  // Function to delete a property from the employee object (non-destructive)
+  function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  
+  // Function to delete a property from the employee object (destructive)
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+  
+  
